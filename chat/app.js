b11b9ddc15c201075f86058b17c91c650c3b535f
robjar <robjar@wp.eu>
2017-01-29 23:06:55 +0100

    basic server

--- chat/app.js
+++ chat/app.js
@@ -1,10 +1,28 @@
 'use strict';
 
+var path = require('path');
+
 var express = require('express');
 var app = express();
+var hogan = require('hogan-express');
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', hogan);
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.route('/').get(function(req, res, next) {
-  res.send('<h1>Hello World!');
+  res.render('index', {title: 'Welcome to MegaChat!'});
+});
+
+app.route('/room').get(function(req, res, next) {
+  res.render('room', {title: 'Room: room_name'});
+});
+
+app.route('/chatroom').get(function(req, res, next) {
+  res.render('chatroom', {title: 'Welcome to MegaChat!'});
 });
 
 app.listen(3000, function() {
