e2a676ac64de5bb1f0fd1af69610c6309617e28f
robjar <robjar@wp.eu>
2017-02-01 00:10:10 +0100

    sessions test

--- chat/router/router.js
+++ chat/router/router.js
@@ -13,4 +13,15 @@ router.get('/chatroom', function(req, res, next) {
   res.render('chatroom', {title: 'Welcome to MegaChat!'});
 });
 
+// session test
+router.get('/setcolor', function(req, res, next) {
+  req.session.favColor = "green";
+  res.send('Setting favourite color!');
+});
+
+router.get('/getcolor', function(req, res, next) {
+  var favColor = req.session.favColor ? req.session.favColor : 'Not found';
+  res.send('Favourite color: ' + favColor);
+});
+
 module.exports = router;
