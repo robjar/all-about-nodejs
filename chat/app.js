d95be0079152dc9dbd193ab783864ad3402433e4
robjar <robjar@wp.eu>
2017-02-08 00:17:17 +0100

    use db as a session store for prod

--- chat/app.js
+++ chat/app.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var path = require('path');
+var extend = require('util')._extend;
 
 var express = require('express');
 var app = express();
 var hogan = require('hogan-express');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var ConnectMongo = require('connect-mongo')(session);
 
 var env = process.env.NODE_ENV || 'development';
 var port = process.env.NODE_PORT || 3000;
@@ -14,23 +16,29 @@ var config = require('./config');
 var router = require('./router/router.js');
 
 
+var sessionConfig = {
+  secret: config.sessionSecret,
+  saveUninitialized: true,
+  resave: true
+};
+
+if (env !== 'development') {
+  sessionConfig = extend(sessionConfig, {
+    store: ConnectMongo({
+      url: config.dbURL,
+      stringify: true
+    })
+  });
+}
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', hogan);
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
+app.use(session(sessionConfig));
 app.use('/', router);
-
-if (env === 'development') {
-  app.use(session({
-    secret: 'topsecret',
-    saveUninitialized: true,
-    resave: true
-  }));
-} else {
-
-}
 
 
 app.listen(3000, function() {
